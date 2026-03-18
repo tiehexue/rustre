351fb30ca292362a3707cfd45d7d8e4dbdf7d2ae
wangyuan <tiehexue@hotmail.com>
2026-03-18 10:13:38 +0800

    updated local mac configs.

---
+++ t.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+
+cargo run -- mgs --log-level debug &
+cargo run -- mds --log-level debug &
+cargo run -- oss -i 0 -l 0.0.0.0:9500 --data-dir /tmp/rustre/oss0 --log-level debug &
+cargo run -- oss -i 1 -l 0.0.0.0:9501 --data-dir /tmp/rustre/oss1 --log-level debug &
+cargo run -- oss -i 2 -l 0.0.0.0:9502 --data-dir /tmp/rustre/oss2 --log-level debug &
+cargo run -- oss -i 3 -l 0.0.0.0:9503 --data-dir /tmp/rustre/oss3 --log-level debug &
