e88952922bcb40a37e7c23b1692e5a05c9f879ae
wangy10 <wangy10@chinatelecom.cn>
2026-03-27 15:10:49 +0800

    opt write in fuse, bigger improvement when cp big file.

--- r.bat
+++ r.bat
@@ -1,5 +1,5 @@
-start rustre mgs -l 172.24.5.23:9400
+start target\release\rustre mgs -l 172.24.5.23:9400
 sleep 2
-start rustre mds -m 172.24.5.23:9400 -l 172.24.5.23:9401
-start rustre oss -i 0 -m 172.24.5.23:9400 -l 172.24.5.23:9500 --data-dir /tmp/rustre/oss0
-start rustre oss -i 1 -m 172.24.5.23:9400 -l 172.24.5.23:9501 --data-dir /tmp/rustre/oss1
+start target\release\rustre mds -m 172.24.5.23:9400 -l 172.24.5.23:9401
+start target\release\rustre oss -i 0 -m 172.24.5.23:9400 -l 172.24.5.23:9500 --data-dir /tmp/rustre/oss0
+start target\release\rustre oss -i 1 -m 172.24.5.23:9400 -l 172.24.5.23:9501 --data-dir /tmp/rustre/oss1
